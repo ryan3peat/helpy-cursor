6691eddd0c0a8d186c91eeb3c5816b6630f445cf
julianoliko-hub <julianoliko@gmail.com>
2025-12-02 11:47:06 +0800

    Fix: Import index.css to restore theme styling

--- index.tsx
+++ index.tsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ClerkProvider } from '@clerk/clerk-react';
+import './index.css';
 import App from './App';
 
 const clerkPubKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
